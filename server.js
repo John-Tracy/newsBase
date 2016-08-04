1ec0116dbdce26841a41ec227dfcfc9b03dfbea7
John Tracy <johnmtracy133@hotmail.com>
2016-08-04 03:21:49 -0400

    mongodb uri

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // Database configuration
 var mongojs = require('mongojs');
-var databaseUrl = "newsScraper";
-var collections = ["scrapedData"];
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
+var databaseUrl = "mongodb://heroku_2b8v8x22:e4ckvcagn9qnsk7bv7vse0mfnn@ds139725.mlab.com:39725/heroku_2b8v8x22";
+// var collections = ["scrapedData"];
+//  mongojs configuration to the db variable
+var db = mongojs(databaseUrl);
 db.on('error', function(err) {
   console.log('Database Error:', err);
 });
